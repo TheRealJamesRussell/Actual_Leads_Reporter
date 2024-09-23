487411f4d208d9f2868b8c0c7c3fd5ba61939060
TheRealJamesRussell <145216038+TheRealJamesRussell@users.noreply.github.com>
2024-09-23 14:48:51 +0200

    fix: add path to python
    Else you get a modulenotfound error for src.main

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 import csv
-import click
 import os
+import sys
 
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+import click
 
 def remove_test_entries(rows: list[dict]) -> list[dict]:
     """
