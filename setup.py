097a2bb2fd69786519890bff3b054bfee991b1a2
TheRealJamesRussell <145216038+TheRealJamesRussell@users.noreply.github.com>
2024-09-23 12:49:45 +0200

    refactor: bump version and formatting

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path  # Import Path from pathlib
 
 setup(
     name="Actual_Leads_Reporter",
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     entry_points={
@@ -21,6 +21,8 @@ setup(
         ],
     },
     python_requires=">=3.6",
-    long_description=(Path(__file__).parent / "README.md").read_text(),  # Add the long description directly
+    long_description=(
+        Path(__file__).parent / "README.md"
+    ).read_text(),  # Add the long description directly
     long_description_content_type="text/markdown",  # Specify the content type of the long description
 )
