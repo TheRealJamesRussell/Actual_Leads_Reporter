d3b0e301c6a8b1388681d3332a7887560bd28961
TheRealJamesRussell <145216038+TheRealJamesRussell@users.noreply.github.com>
2024-09-23 13:18:29 +0200

    feat: remove duplicates
    to handle a csv where there might be leads with the same email or
    phone number, remove duplicates and write to a new csv

--- src/main.py
+++ src/main.py
@@ -1,4 +1,39 @@
 import csv
+from collections import defaultdict
+
+
+def dedupe_csv(file_path: str) -> str:
+    """
+    Remove duplicates based on phone number or email, and write to a new CSV.
+
+    :param file_path: The path to the original CSV file.
+    :return: The path to the deduped CSV file.
+    """
+    unique_emails = {}
+    unique_phones = {}
+    output_file_path = f"deduped_{file_path}"
+
+    # Open the file with utf-8-sig encoding to handle and remove BOM
+    with open(file_path, mode="r", newline="", encoding="utf-8-sig") as file:
+        reader = csv.DictReader(file)
+        fieldnames = reader.fieldnames
+        for row in reader:
+            email = row.get("Email")
+            phone = row.get("Phone Number")
+            # Check for uniqueness based on email or phone number
+            if email not in unique_emails and phone not in unique_phones:
+                unique_emails[email] = row
+                unique_phones[phone] = row
+
+    # Open the output file with utf-8-sig encoding to ensure BOM is handled correctly
+    with open(output_file_path, mode="w", newline="", encoding="utf-8-sig") as file:
+        writer = csv.DictWriter(file, fieldnames=fieldnames, quoting=csv.QUOTE_ALL)
+        writer.writeheader()
+        # Write unique entries based on email
+        for entry in unique_emails.values():
+            writer.writerow(entry)
+
+    return output_file_path
 
 
 def get_total_leads_from_csv(file_path: str) -> int:
@@ -31,13 +66,21 @@ def calculate_lead_metrics(
 
 
 if __name__ == "__main__":
-    # Example usage for get_total_leads_from_csv
-    file_path = "test.csv"
-    total_leads = get_total_leads_from_csv(file_path)
-    print(f"Total Leads: {total_leads}")
+    # Example usage for deduping and getting total leads from deduped CSV
+    file_path : str = "test copy.csv"
+    deduped_file_path : str = dedupe_csv(file_path)
+    total_leads : int = get_total_leads_from_csv(deduped_file_path)
+    print(f"Total Leads from deduped file: {total_leads}")
 
     # Example usage for calculate_lead_metrics
-    total_spend, total_leads, cost_per_lead = calculate_lead_metrics(total_leads, 911.26)
+    total_spend: float
+    total_leads: int
+    cost_per_lead: float
+
+    total_spend, total_leads, cost_per_lead = calculate_lead_metrics(
+        total_leads, 911.26
+    )
+
     print(
         f"Total Spend: {total_spend}, Total Leads: {total_leads}, Cost per Lead: {cost_per_lead}"
     )
